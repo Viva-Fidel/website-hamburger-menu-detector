a028fb09dc2e213650533c10057f7c8ee9e5afb4
Aleksei Artamonov <alexis.artamonov@gmail.com>
2023-03-10 13:48:06 +0300

    minor changes

--- detection.py
+++ detection.py
@@ -22,7 +22,7 @@ class Detection:
                 x1, y1, x2, y2 = int(row[0] * x_shape), int((row[1] * y_shape)), int(
                     row[2] * x_shape), int((row[3] * y_shape))
 
-                cv2.rectangle(self.img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(self.img, (x1+10, y1+10), (x2+10, y2+10), (255, 0, 0), 2)
                 return self.img
             else:
                 return self.img
